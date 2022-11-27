c4c7cefcb9b8faceeb7b058f261428d62ab2ea22
”danriz” <farukcevik@gmail.com>
2022-11-27 15:00:52 +0300

    “reactProject”

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import City from './City';
 function App() {
+  const key="1ce9c19dc266c59585b12096cabf85d2";
+  const [search,setSearch]=useState("");
+  const [city,setCity]=useState();
+useEffect(() => {
+
+  async function getApi() {
+
+
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`
+        );
+      console.log(response);
+      setCity(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+getApi();
+},[search])
+console.log(search);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     hello
+     <div>
+     <input onChange={(e)=> setSearch(e.target.value)}
+     className="border-8" type="text"/>
+     
+     <City city={city}/>
+     </div>
+
+
     </div>
+
+
+
   );
 }
 
---
+++ src/City.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const City = ({city}) => {
+    console.log(city);
+  return (
+    <div>
+<h1 className="text-5xl">{city.main.temp}</h1>
+<h1 className="text-5xl">{city.name}</h1>
+<h1 className="text-5xl">{city.weather[0].main}</h1>
+
+
+    </div>
+  )
+}
+
+export default City;
